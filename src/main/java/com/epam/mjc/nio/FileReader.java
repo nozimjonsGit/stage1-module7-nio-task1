2911dd12dd302457ea7e7d572ff63563c8784966
Nozimjon Nabiev <73819566+NozimjonNabiev@users.noreply.github.com>
2023-07-13 22:26:41 +0500

    Done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,37 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = null;
+        int age = 0;
+        String email = null;
+        long phone = 0L;
+
+        try {
+            String data = Files.readString(file.toPath());
+            String[] lines = data.split(System.lineSeparator());
+
+            for (String line : lines) {
+                if (line.startsWith("Name: ")) {
+                    name = line.substring("Name: ".length());
+                } else if (line.startsWith("Age: ")) {
+                    age = Integer.parseInt(line.substring("Age: ".length()));
+                } else if (line.startsWith("Email: ")) {
+                    email = line.substring("Email: ".length());
+                } else if (line.startsWith("Phone: ")) {
+                    phone = Long.parseLong(line.substring("Phone: ".length()));
+                }
+            }
+
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+        return new Profile(name, age, email, phone);
     }
 }
